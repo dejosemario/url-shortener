4694835c4d9b65f21b4d560e3eebc9f2275f7a74
dejosemario <josemariaodumodu@gmail.com>
2024-09-24 20:19:04 +0100

    fix: adjust the clickable links to go to a different tab

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const shortUrl = require('./models/shortUrl');
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener', {
-    useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index');
-    }
-);
-
-app.post('/shortUrls', async (req, res) => {
-   await shortUrl.create({ full: req.body.fullUrl });
-   res.redirect('/');
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log('Server is running');
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect;
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running");
 });
